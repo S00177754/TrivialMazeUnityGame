2c0178867d4a1eaee7dac89420ee5b0f41b86a85
Ronan Cassidy <ronancassidy.work@gmail.com>
2020-01-09 11:25:41 +0000

    Login Setup

--- Trivial Maze/Assets/Ronan/Scripts/LoginSystem/LoginController.cs
+++ Trivial Maze/Assets/Ronan/Scripts/LoginSystem/LoginController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class LoginController : MonoBehaviour
 {
     //UI Panels
-    
+    public GameObject LoginPanel;
+    public GameObject SignUpPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SwitchToLogin();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwitchToLogin()
+    {
+        LoginPanel.SetActive(true);
+        SignUpPanel.SetActive(false);
+    }
+
+    public void SwitchToSignUp()
+    {
+        SignUpPanel.SetActive(true);
+        LoginPanel.SetActive(false);
     }
 }
