9d58b7ff282c9db2574dd72f3b3a0ec71b23752c
Ronan Cassidy <ronancassidy.work@gmail.com>
2020-01-10 13:15:04 +0000

    Incorporated Looking Script
    Created script to control player camera, moving onto movement next. Make note of unity documentation for joysticks.

---
+++ Trivial Maze/Assets/Ronan/Scripts/PlayerLookController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookController : MonoBehaviour
+{
+    public float SensitivitySpeed = 80f;
+    public Transform playerTransform;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX_Axis = Input.GetAxis("Mouse X") * SensitivitySpeed * Time.deltaTime;
+        float MouseY_Axis = Input.GetAxis("Mouse Y") * SensitivitySpeed * Time.deltaTime;
+
+        xRotation -= MouseY_Axis;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerTransform.Rotate(Vector3.up * MouseX_Axis);
+    }
+}
---
+++ Trivial Maze/Assets/Ronan/Scripts/PlayerMovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        
+
+    }
+}
