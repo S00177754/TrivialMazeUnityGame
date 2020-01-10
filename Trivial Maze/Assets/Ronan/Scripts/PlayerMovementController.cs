5dc65b40274b7326848f293c6e9e0cbe7d3f130d
Ronan Cassidy <ronancassidy.work@gmail.com>
2020-01-10 13:58:27 +0000

    Beginning gravity
    Basic gravity added but velocity keeps increasing, need to check when in touch with ground. Maybe a raycast?

--- Trivial Maze/Assets/Ronan/Scripts/PlayerMovementController.cs
+++ Trivial Maze/Assets/Ronan/Scripts/PlayerMovementController.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    Camera camera;
+    //Public 
+    public CharacterController characterController;
+    public float Speed = 10f;
+    public float SprintSpeed = 14f;
+    public float Gravity = -9.81f;
+    //Private
+    Vector3 velocity;
 
     private void Start()
     {
-        camera = Camera.main;
     }
 
     private void Update()
     {
-        
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
 
+        Vector3 direction = transform.right * xMove + transform.forward * zMove;
+
+        if (Input.GetButton("Sprint"))//Sprint setup in input axis
+        {
+            characterController.Move(direction * Time.deltaTime * SprintSpeed);
+        }
+        else
+        {
+            characterController.Move(direction * Time.deltaTime * Speed);
+        }
+
+        velocity.y += Gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime); //T = time.deltaTime
     }
+
+    // y = ((1/2)*g) * t^2
+    //Velocity constantly increasing, need to reset when hits the ground. Maybe some kind of check with reference to the bottom of the object
 }
