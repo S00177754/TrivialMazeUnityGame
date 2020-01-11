ac03d87757ac4c2e23c558a5c8818a6565e45a4f
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-01-11 19:33:02 +0000

    Player Movement Update
    Added full jump and proper gravity.
    Partial addition of climbing though still a few issues to figure out

--- Trivial Maze/Assets/Ronan/Scripts/PlayerLookController.cs
+++ Trivial Maze/Assets/Ronan/Scripts/PlayerLookController.cs
@@ -7,26 +7,34 @@ public class PlayerLookController : MonoBehaviour
     public float SensitivitySpeed = 80f;
     public Transform playerTransform;
     float xRotation = 0f;
+    public bool LockedView;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        LockedView = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
         //Move up into variables?
         float MouseX_Axis = Input.GetAxis("Mouse X") * SensitivitySpeed * Time.deltaTime;
         float MouseY_Axis = Input.GetAxis("Mouse Y") * SensitivitySpeed * Time.deltaTime;
 
         xRotation -= MouseY_Axis; //Addinbg to it gives opposite direction
-
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Clamp to 90 deg
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //applies rotation in order of z,x,y
 
-        playerTransform.Rotate(Vector3.up * MouseX_Axis);
+        if (!LockedView)
+        {
+            playerTransform.Rotate(Vector3.up * MouseX_Axis);
+        }
+        
     }
 }
--- Trivial Maze/Assets/Ronan/Scripts/PlayerMovementController.cs
+++ Trivial Maze/Assets/Ronan/Scripts/PlayerMovementController.cs
@@ -6,11 +6,26 @@ public class PlayerMovementController : MonoBehaviour
 {
     //Public 
     public CharacterController characterController;
+
     public float Speed = 10f;
     public float SprintSpeed = 14f;
+    public float climbSpeed = 3f;
+    public float JumpHeight = 2f;
+
     public float Gravity = -9.81f;
+    public Transform bottomOfPlayer;
+    public Transform frontOfPlayer;
+    public PlayerLookController playerLookController;
+    public LayerMask groundLayer;
+    public LayerMask defaultLayer;
+    public float GroundCheckRadius = 0.3f;
+    public float ClimbCheckRadius = 0.3f;
+
     //Private
     Vector3 velocity;
+    bool isTouchingGround;
+    bool isTouchingClimbable;
+    RaycastHit hit;
 
     private void Start()
     {
@@ -18,25 +33,102 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Update()
     {
+        hit = new RaycastHit();
+
+        //Check if touching ground at the start of the update.
+        //Tried with raycast but possibly too small of a check, only checks a pinpoint instead of a small area, how about checking a radius?
+        isTouchingGround = Physics.CheckSphere(bottomOfPlayer.position, GroundCheckRadius, groundLayer);
+
+        //Reset velocity
+        if(isTouchingGround && velocity.y < 0)
+        {
+            velocity.y = -2f; //works but still seems a little floaty, going to try and incorporate mass into formula   
+        }
+
+        #region Movement X and z
+        //Take input and move by it
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
 
         Vector3 direction = transform.right * xMove + transform.forward * zMove;
 
-        if (Input.GetButton("Sprint"))//Sprint setup in input axis
+        if (!isTouchingClimbable)
         {
-            characterController.Move(direction * Time.deltaTime * SprintSpeed);
+            if (Input.GetButton("Sprint"))//Sprint setup in input axis
+            {
+                characterController.Move(direction * Time.deltaTime * SprintSpeed);
+            }
+            else
+            {
+                characterController.Move(direction * Time.deltaTime * Speed);
+            }
         }
-        else
+        //else if(isTouchingClimbable && isTouchingGround)
+        //{
+        //    if (Input.GetButton("Sprint"))//Sprint setup in input axis
+        //    {
+        //        characterController.Move(direction * Time.deltaTime * SprintSpeed);
+        //    }
+        //    else
+        //    {
+        //        characterController.Move(direction * Time.deltaTime * Speed);
+        //    }
+        //}
+        #endregion
+
+        #region Jump
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
         {
-            characterController.Move(direction * Time.deltaTime * Speed);
+            velocity.y = Mathf.Sqrt(JumpHeight * -2 * Gravity); //v = Square root of (h * -2 * g)
         }
 
-        velocity.y += Gravity * Time.deltaTime;
+        #endregion
 
-        characterController.Move(velocity * Time.deltaTime); //T = time.deltaTime
+        #region Climb
+
+        
+        //if(Physics.Raycast(frontOfPlayer.position, frontOfPlayer.up, out hit, ClimbCheckRadius, defaultLayer))
+        //{
+        //    Debug.Log("Did Hit");
+
+        //    if(hit.collider.gameObject.tag == "Climbable")
+        //    {
+        //        Debug.Log("Climbable");
+        //        isTouchingClimbable = true;
+        //        playerLookController.LockedView = true;
+        //    }
+            
+        //}
+        //else
+        //{
+        //    isTouchingClimbable = false;
+        //    playerLookController.LockedView = false;
+        //}
+
+
+        ////Climbing movement
+        //if (isTouchingClimbable)
+        //{
+        //    characterController.Move((transform.up * zMove + transform.right * xMove) * climbSpeed * Time.deltaTime);
+        //}
+
+
+        #endregion
+
+        if (!isTouchingClimbable)
+        {
+            velocity.y += 2 * Gravity * Time.deltaTime; // y = ((1/2)*g) * t^2
+
+            characterController.Move(velocity * Time.deltaTime); //T = time.deltaTime
+        }
     }
 
-    // y = ((1/2)*g) * t^2
+
     //Velocity constantly increasing, need to reset when hits the ground. Maybe some kind of check with reference to the bottom of the object
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(frontOfPlayer.position, frontOfPlayer.up,Color.magenta);
+    }
 }
